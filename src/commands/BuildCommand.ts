72681f99134eab813efb17da000787035a65adf5
Haze Lee <hazelee@realignist.me>
2018-10-24 11:05:03 +0900

    add: github token for api limit

--- src/commands/BuildCommand.ts
+++ src/commands/BuildCommand.ts
@@ -1,9 +1,11 @@
 import { BuildImageCommand, TagValue, OptionModel } from '@evidnet/scv-core'
+import { githubKey as token } from '../../secret.json'
 
 export class BuildCommand extends BuildImageCommand {
   substituteMap: Map<string, TagValue> = new Map<string, TagValue>([
     ['@base', 'amd64/ubuntu:18.04'],
-    ['@tag', (tag: string) => tag]
+    ['@tag', (tag: string) => tag],
+    ['@token', token]
   ])
 
   tags: string[] = ['3.3.3', '3.4.0', '3.4.1', '3.4.2', '3.4.3', '3.4.4', '3.5.0', '3.5.1']
